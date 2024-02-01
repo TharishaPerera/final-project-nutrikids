751b2cf47bc6b9409a351df1d2ac970c4b14c327
TharishaPerera <tharishaperera@gmail.com>
2024-02-01 21:59:08 +0530

    add auth error page with common error card

---
+++ components/common/error-card.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+
+interface ErrorCardProps {
+    title?: string
+    description?: string
+    url?: string
+}
+
+export const ErrorCard: React.FC<ErrorCardProps> = ({ title = "Something went wrong!", description = "Sorry about that! Please visit our homepage to get where you need togo.", url = "/" }) => {
+  return (
+    <div className=" block space-y-6 text-center">
+      <div>
+        <h1 className="text-3xl md:text-4xl font-semibold">
+          {title}
+        </h1>
+      </div>
+      <div className="space-y-6">
+        <h3 className="text-xl md:text-2xl ">
+            {description}
+        </h3>
+        <div className="">
+          <Link href={url} className={cn(buttonVariants({ variant: "outline", size: "lg" }))}>
+            <>
+              <ChevronLeft className="mr-2 h-4 w-4" />
+              Back
+            </>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
