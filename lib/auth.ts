414b30547f145ff1382dd874a7e8536b4fecbe48
TharishaPerera <tharishaperera@gmail.com>
2024-02-02 00:14:32 +0530

    improved comments

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 
 /**
  * Return current user data
+ * For sever components
  */
 export const currentUser = async () => {
   const session = await auth();
@@ -11,6 +12,7 @@ export const currentUser = async () => {
 
 /**
  * Return current user role
+ * For sever components
  */
 export const currentRole = async () => {
   const session = await auth();
