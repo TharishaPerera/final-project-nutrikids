7ee6c9606dce55dedbc17518a8e766e39389757a
TharishaPerera <tharishaperera@gmail.com>
2024-02-03 22:53:48 +0530

    add pediatricians page

---
+++ testdata/pediatricians.ts
@@ -0,0 +1,39 @@
+// TODO: remove test data
+export const pediatrician = [
+  {
+    id: "1",
+    name: "adjskd dfdsf",
+    specialization: "jdfkjasdf",
+    image: "https://xsgames.co/randomusers/assets/avatars/male/72.jpg",
+  },
+  {
+    id: "2",
+    name: "adjskd dfasdfadf",
+    specialization: "jdfkjasdf",
+    image: "https://xsgames.co/randomusers/assets/avatars/male/16.jpg",
+  },
+  {
+    id: "3",
+    name: "adjskddfsadf ds",
+    specialization: "jdfkjasdf",
+    image: "https://xsgames.co/randomusers/assets/avatars/male/76.jpg",
+  },
+  {
+    id: "4",
+    name: "adjskd",
+    specialization: "jdfkjasdf",
+    image: "https://xsgames.co/randomusers/assets/avatars/male/74.jpg",
+  },
+  {
+    id: "5",
+    name: "adjskd",
+    specialization: "jdfkjasdf",
+    image: "https://xsgames.co/randomusers/assets/avatars/male/42.jpg",
+  },
+  {
+    id: "6",
+    name: "adjskd",
+    specialization: "jdfkjasdf",
+    image: "https://xsgames.co/randomusers/assets/avatars/male/8.jpg",
+  },
+];
