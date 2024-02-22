7a27b105fd89cab8de189898d9c20938efdca8dc
TharishaPerera <tharishaperera@gmail.com>
2024-02-22 22:57:49 +0530

    added smtp email functionality

---
+++ components/email-templates/two-factor-code.ts
@@ -0,0 +1,188 @@
+export const TwoFactorCodeTemplate = `
+<!DOCTYPE html>
+<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"><!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]--><!--[if !mso]><!-->
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900" rel="stylesheet" type="text/css"><!--<![endif]-->
+	<style>
+		* {
+			box-sizing: border-box;
+		}
+
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		a[x-apple-data-detectors] {
+			color: inherit !important;
+			text-decoration: inherit !important;
+		}
+
+		#MessageViewBody a {
+			color: inherit;
+			text-decoration: none;
+		}
+
+		p {
+			line-height: inherit
+		}
+
+		.desktop_hide,
+		.desktop_hide table {
+			mso-hide: all;
+			display: none;
+			max-height: 0px;
+			overflow: hidden;
+		}
+
+		.image_block img+div {
+			display: none;
+		}
+
+		@media (max-width:620px) {
+			.desktop_hide table.icons-inner {
+				display: inline-block !important;
+			}
+
+			.icons-inner {
+				text-align: center;
+			}
+
+			.icons-inner td {
+				margin: 0 auto;
+			}
+
+			.mobile_hide {
+				display: none;
+			}
+
+			.row-content {
+				width: 100% !important;
+			}
+
+			.stack .column {
+				width: 100%;
+				display: block;
+			}
+
+			.mobile_hide {
+				min-height: 0;
+				max-height: 0;
+				max-width: 0;
+				overflow: hidden;
+				font-size: 0px;
+			}
+
+			.desktop_hide,
+			.desktop_hide table {
+				display: table !important;
+				max-height: none !important;
+			}
+		}
+	</style>
+</head>
+
+<body style="background-color: #ffffff; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+	<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;">
+		<tbody>
+			<tr>
+				<td>
+					<table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+						<tbody>
+							<tr>
+								<td>
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 600px; margin: 0 auto;" width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+													<table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td class="pad" style="width:100%;padding-right:0px;padding-left:0px;">
+																<div class="alignment" align="center" style="line-height:10px">
+																	<div style="max-width: 120px;"><a href="nutrikids.tharishaperera.com.lk" target="_blank" style="outline:none" tabindex="-1"><img src="https://957b9bbb0c.imgdist.com/public/users/Integrators/BeeProAgency/1151141_1136821/favicon.ico" style="display: block; height: auto; border: 0; width: 100%;" width="120" alt="I'm an image" title="I'm an image"></a></div>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table class="heading_block block-2" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td class="pad">
+																<h1 style="margin: 0; color: #000000; direction: ltr; font-family: 'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif; font-size: 38px; font-weight: 700; letter-spacing: 1px; line-height: 120%; text-align: center; margin-top: 0; margin-bottom: 0; mso-line-height-alt: 45.6px;"><span class="tinyMce-placeholder"><a href="nutrikids.tharishaperera.com.lk" target="_blank" style="text-decoration: none; color: #000000;" rel="noopener">NutriKids</a></span></h1>
+															</td>
+														</tr>
+													</table>
+													<div class="spacer_block block-3" style="height:30px;line-height:30px;font-size:1px;">&#8202;</div>
+													<table class="paragraph_block block-4" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad">
+																<div style="color:#101112;direction:ltr;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;font-size:20px;font-weight:700;letter-spacing:0px;line-height:120%;text-align:center;mso-line-height-alt:24px;">
+																	<p style="margin: 0;">Your Two-Factor Authentication Code</p>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<table class="paragraph_block block-5" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
+														<tr>
+															<td class="pad">
+																<div style="color:#101112;direction:ltr;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;font-size:30px;font-weight:700;letter-spacing:10px;line-height:120%;text-align:center;mso-line-height-alt:36px;">
+																	<p style="margin: 0;">{{code}}</p>
+																</div>
+															</td>
+														</tr>
+													</table>
+													<div class="spacer_block block-6" style="height:100px;line-height:100px;font-size:1px;">&#8202;</div>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;">
+						<tbody>
+							<tr>
+								<td>
+									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 600px; margin: 0 auto;" width="600">
+										<tbody>
+											<tr>
+												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+													<table class="icons_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+														<tr>
+															<td class="pad" style="vertical-align: middle; color: #1e0e4b; font-family: 'Inter', sans-serif; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;">
+																<table width="100%" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+																	<tr>
+																		<td class="alignment" style="vertical-align: middle; text-align: center;"><!--[if vml]><table align="center" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
+																			<!--[if !vml]><!-->
+																			<table class="icons-inner" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; margin-right: -4px; padding-left: 0px; padding-right: 0px;" cellpadding="0" cellspacing="0" role="presentation"><!--<![endif]-->
+																				<tr>
+																					<td style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;"><a href="http://designedwithbeefree.com/" target="_blank" style="text-decoration: none;"><img class="icon" alt="Beefree Logo" src="https://d1oco4z2z1fhwp.cloudfront.net/assets/Beefree-logo.png" height="32" width="34" align="center" style="display: block; height: auto; margin: 0 auto; border: 0;"></a></td>
+																					<td style="font-family: 'Inter', sans-serif; font-size: 15px; font-weight: undefined; color: #1e0e4b; vertical-align: middle; letter-spacing: undefined; text-align: center;"><a href="http://designedwithbeefree.com/" target="_blank" style="color: #1e0e4b; text-decoration: none;">Designed with Beefree</a></td>
+																				</tr>
+																			</table>
+																		</td>
+																	</tr>
+																</table>
+															</td>
+														</tr>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+	</table><!-- End -->
+</body>
+
+</html>
+`
--- lib/smtp.ts
+++ lib/smtp.ts
@@ -1,35 +1,89 @@
 "use server"
 
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
+import handlebars from "handlebars";
+import { TwoFactorCodeTemplate } from "@/components/email-templates/two-factor-code";
+import { ResetPasswordTemplate } from "@/components/email-templates/reset-password";
+import { VerifyEmailTemplate } from "@/components/email-templates/verify-email";
 
-export const sendTestEmail = async (email: string) => {
-    const { SMPT_EMAIL, SMTP_PASSWORD } = process.env
+const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
+const domain = process.env.NEXT_PUBLIC_APP_URL;
 
-    const transport = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: SMPT_EMAIL,
-            pass: SMTP_PASSWORD
-        }
-    })
+// nodemailer transporter defined
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: SMTP_EMAIL,
+    pass: SMTP_PASSWORD,
+  },
+});
 
-    try {
-        const testResult = await transport.verify()
-        console.log(testResult)
-    } catch (error) {
-        console.log("ERROR1: ", error)
-        return
-    }
+/**
+ * Send two factor authentication code email
+ */
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  // create template
+  const template = handlebars.compile(TwoFactorCodeTemplate);
+  const body = template({
+    code: token,
+  });
 
-    try {
-        const sendResult = await transport.sendMail({
-            from: SMPT_EMAIL,
-            to: "tharishaperera@gmail.com",
-            subject: "Test Subject",
-            html: "Test Body"
-        })
-        console.log(sendResult)
-    } catch (error) {
-        console.log("ERROR2: ", error)
-    }
-}
+  try {
+    await transport.sendMail({
+      from: SMTP_EMAIL,
+      to: email,
+      subject: "Two-Factor Authentication Code",
+      html: body,
+    });
+  } catch (error) {
+    console.log("2FA-CODE: ", error);
+  }
+};
+
+/**
+ * Send reset password link email
+ */
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
+
+  // create template
+  const template = handlebars.compile(ResetPasswordTemplate);
+  const body = template({
+    resetLink: resetLink,
+  });
+
+  try {
+    await transport.sendMail({
+      from: SMTP_EMAIL,
+      to: email,
+      subject: "Reset Password",
+      html: body,
+    });
+  } catch (error) {
+    console.log("Reset Password: ", error);
+  }
+};
+
+/**
+ * Send account verification email
+ */
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+
+  // create template
+  const template = handlebars.compile(VerifyEmailTemplate);
+  const body = template({
+    confirmLink: confirmLink,
+  });
+
+  try {
+    await transport.sendMail({
+      from: SMTP_EMAIL,
+      to: email,
+      subject: "Confirm Your Email",
+      html: body,
+    });
+  } catch (error) {
+    console.log("Confirm Email: ", error);
+  }
+};
