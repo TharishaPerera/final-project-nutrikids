e1e057c67bfbdbf47dea9b8c275d294ff647c388
TharishaPerera <tharishaperera@gmail.com>
2024-02-23 19:05:16 +0530

    added global site name

--- app/(community)/community/page.tsx
+++ app/(community)/community/page.tsx
@@ -1,10 +1,11 @@
 import { CommunityFeed } from "@/components/community/community-feed-section";
 import { NewPostForm } from "@/components/form/community/new-post";
+import { ApplicationName } from "@/config/navlinks.config";
 
 const CommunityPage = () => {
   return (
     <div className="w-full space-y-4 p-2">
-      <h1 className="text-xl font-medium">{Si} Community</h1>
+      <h1 className="text-xl font-medium">{ApplicationName} Community</h1>
       <div className="">
         <NewPostForm />
       </div>
