94de6031b26951cb2dc4d809fc0df877dd3d64f0
TharishaPerera <tharishaperera@gmail.com>
2024-03-27 22:00:03 +0530

    added userLevel hook and role based side navigation

--- hooks/use-current-role.ts
+++ hooks/use-current-role.ts
@@ -8,3 +8,12 @@ export const useCurrentRole = () => {
   const session = useSession();
   return session.data?.user.role;
 };
+
+/***
+ * Get only user current level from the session
+ * For client components
+ */
+export const useCurrentLevel = () => {
+  const session = useSession();
+  return session.data?.user.level;
+};
