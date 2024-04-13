ced6e355739b1b07a622e21ad618f046d01aeff5
TharishaPerera <tharishaperera@gmail.com>
2024-04-13 13:44:39 +0530

    restore history page

--- app/(root)/history/page.tsx
+++ app/(root)/history/page.tsx
@@ -1,29 +1,9 @@
-"use client";
-
-import { createMeeting } from "@/actions/appointment/daily.co/meeting";
-import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
-import { toast } from "sonner";
+import React from 'react'
 
 const HistoryPage = () => {
-  const [meetingLink, setMeetingLink] = useState<string | undefined>("");
-
-  const createNewMeeting = async () => {
-    createMeeting().then((res) => {
-      if (res.error) {
-        console.log(res.error);
-        toast.error(res.error);
-      } else {
-        setMeetingLink(res.url);
-      }
-    });
-  };
   return (
-    <>
-      <Button onClick={createNewMeeting}>Create Meeting</Button>
-      {meetingLink && <p>{meetingLink}</p>}
-    </>
-  );
-};
+    <div>HistoryPage</div>
+  )
+}
 
-export default HistoryPage;
+export default HistoryPage
