74efd11eaba60f9b04102278cf738dda90661464
TharishaPerera <tharishaperera@gmail.com>
2024-05-15 13:44:24 +0530

    added upcoming activities table and format timeslots

--- components/common/dashboard-table.tsx
+++ components/common/dashboard-table.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-} from "@/components/ui/card";
+"use client";
+
+import React, { useEffect, useState, useTransition } from "react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -12,8 +10,58 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  GetUpcomingActivitiesForAdmin,
+  GetUpcomingActivitiesForOthers,
+} from "@/actions/dashboard/activities";
+import { toast } from "sonner";
+import { dateFormat, formatTimeSlot } from "@/lib/utils";
+import { useMounted } from "@/hooks/use-mounted";
+import { useCurrentLevel } from "@/hooks/use-current-role";
+
+export interface Activities {
+  appointmentDate: Date;
+  timeslot: string;
+  child: {
+    name: string;
+  };
+  parent?: {
+    name: string | null;
+    email: string | null;
+  };
+}
 
 export const DashboardTable = () => {
+  const [activities, setActivities] = useState<Activities[]>([]);
+  const [isPending, startTransition] = useTransition();
+  const userLevel = useCurrentLevel();
+
+  useEffect(() => {
+    startTransition(() => {
+      if (userLevel >= 5000) {
+        GetUpcomingActivitiesForAdmin().then((response) => {
+          if (response.error) {
+            toast.error(response.error);
+          }
+          if (response.activities) {
+            setActivities(response.activities);
+          }
+        });
+      } else {
+        GetUpcomingActivitiesForOthers().then((response) => {
+          if (response.error) {
+            toast.error(response.error);
+          }
+          if (response.activities) {
+            setActivities(response.activities);
+          }
+        });
+      }
+    });
+  }, []);
+
+  const mounted = useMounted();
+
   return (
     <Card className="xl:col-span-2">
       <CardHeader className="bg-secondary p-4 text-lg font-semibold border-b">
@@ -29,57 +77,29 @@ export const DashboardTable = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="py-2">
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="py-2">2023-06-23</TableCell>
-              <TableCell className="py-2">15:00 - 16:00</TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className="py-2">
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="py-2">2023-06-23</TableCell>
-              <TableCell className="py-2">15:00 - 16:00</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="py-2">
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="py-2">2023-06-23</TableCell>
-              <TableCell className="py-2">15:00 - 16:00</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="py-2">
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="py-2">2023-06-23</TableCell>
-              <TableCell className="py-2">15:00 - 16:00</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className="py-2">
-                <div className="font-medium">Liam Johnson</div>
-                <div className="hidden text-sm text-muted-foreground md:inline">
-                  liam@example.com
-                </div>
-              </TableCell>
-              <TableCell className="py-2">2023-06-23</TableCell>
-              <TableCell className="py-2">15:00 - 16:00</TableCell>
-            </TableRow>
+            {activities.length <= 0 && (
+              <TableRow>
+                <TableCell colSpan={3} className="py-2 text-muted-foreground">No upcoming activities</TableCell>
+              </TableRow>
+            )}
+            {activities.map((activity, index) => (
+              <TableRow>
+                <TableCell className="py-2">
+                  <div className="font-medium">{activity.child.name}</div>
+                  {activity.parent && (
+                    <div className="hidden text-sm text-muted-foreground md:inline">
+                      {activity.parent.name} - {activity.parent.email}
+                    </div>
+                  )}
+                </TableCell>
+                <TableCell className="py-2">
+                  {dateFormat(activity.appointmentDate, "D MMM, YYYY")}
+                </TableCell>
+                <TableCell className="py-2">
+                  {formatTimeSlot(activity.timeslot)}
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </CardContent>
--- lib/utils.ts
+++ lib/utils.ts
@@ -53,3 +53,24 @@ export const truncateText = (text: string, maxLength: number) => {
   }
   return text;
 }
+
+/**
+ * Format timeslots
+ * @param timeSlotString string
+ * @returns string format (8:00 AM - 5:00 PM)
+ */
+export function formatTimeSlot(timeSlotString: string): string {
+  const [start, end] = timeSlotString.split('-').map(Number);
+
+  const formatTime = (timestamp: number): string => {
+      const date = new Date(timestamp);
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const ampm = hours >= 12 ? 'PM' : 'AM';
+      const formattedHours = hours % 12 || 12;
+      const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+      return `${formattedHours}:${formattedMinutes} ${ampm}`;
+  };
+
+  return `${formatTime(start)} - ${formatTime(end)}`;
+}
